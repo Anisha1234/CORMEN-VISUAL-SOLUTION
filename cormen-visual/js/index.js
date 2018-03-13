320698e1c310da87c305b5e585b0d5d914c28ec8
Anisha Swain <anisha.swain@yahoo.in>
2018-03-13 15:28:48 +0530

    front page done

--- cormen-visual/js/index.js
+++ cormen-visual/js/index.js
@@ -2,20 +2,20 @@ var svg= d3.select('body')
 		  .select('#trans')
 		  .append('svg')
 		  .attr('height',600)
-		  .attr('width',700)
+		  .attr('width',1430)
 		  .attr('x',0)
 		  .attr('y',0)
-for (var i = 0; i < 10; i++) {
-	for (var j = 0; j < 15; j++) {
+for (var i = 0; i < 15; i++) {
+	for (var j = 0; j < 20; j++) {
 	 d3.selectAll('svg').append("rect")
 	    .attr("rx",100)
 	    .attr("ry",100)
 	    .attr("x",function() {
-	    	var gap = 100/16
+	    	var gap = 100/21
 	    	return gap*j + "%"
 	    })
 	    .attr("y",function(){
-	    	var gap = 100/11
+	    	var gap = 100/16
 	    	return gap*i +5+ "%"
 	    })
 	    .attr("width",23)
@@ -28,11 +28,12 @@ var repeated=function() {
               d3.select(this)
                     .transition()
                     .delay(function(d, i) {
-                      return i * 100;
+                      return i * 150;
                     })
                     .duration(1000)
                     .attr("rx",100)
 	    			.attr("ry",100)
+	    			.attr("fill","gray")
 	    			.attr("width",23)
 					.attr("height",23)
 	    			.on("end",function(){
@@ -40,11 +41,12 @@ var repeated=function() {
 	    					d3.select(this)
 		                    .transition()
 	                        .delay(function(d, i) {
-	                          return i * 100;
+	                          return i * 150;
 	                        })
 		                    .duration(1000)
 		                    .attr("rx",0)
 			    			.attr("ry",0)
+			    			.attr("fill","lightblue")
 			    			.attr("width",30)
 							.attr("height",30)
 			    			.on("end",repeated);
@@ -56,18 +58,31 @@ var repeated=function() {
 d3.selectAll('rect')
     .transition()
     .delay(function(d, i) {
-            return i * 100;
+            return i * 150;
           })
     .duration(1000)
     .attr("rx",0)
     .attr("ry",0)
+    .attr("fill","lightblue")
     .attr("width",30)
 	.attr("height",30)
     .on("end",repeated);
+svg.append('rect')
+		.attr("width", 570)
+       	.attr("height", 800)
+       	.attr("x", "50%")
+       	.attr("y", "0%")
+       	.attr("fill","white")
 svg.append("svg:image")
               .attr("xlink:href", "./image/1.png")
               .attr("width", 570)
-              .attr("height", 300)
-              .attr("x", "6%")
-              .attr("y", "17%")
+              .attr("height", 400)
+              .attr("x", "50%")
+              .attr("y", "54%")
+svg.append("svg:image")
+              .attr("xlink:href", "./image/1.jpg")
+              .attr("width", 570)
+              .attr("height", 450)
+              .attr("x", "50%")
+              .attr("y", "2%")
               
